56555e8b5c4a2262f11eafb255a1891a4d6db1a2
luisviniciusfco <luis2001vini@hotmail.com>
2024-06-03 19:28:36 -0300

    commit

--- App.js
+++ App.js
@@ -1,217 +1,148 @@
-import React, { useState } from 'react';
-import { Image, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import logo from './assets/cara1.png';
-import logo1 from './assets/lv.png';
-
-
-// Componentes de página
-const PaginaInicial = () => {
-  return (
-    <View style={styles.pagina}>
-      <Text style={styles.textoPagina}>
-      <Image style={styles.images} source={ logo1 }/> {"\n\n\n\n"}
-        {"\n"}Nome: Luis Vinicius F. Silva
-        {"\n"}Idade - 23 Anos
-        {"\n"}Ocupação - Estudante
-      </Text>
-    </View>
-  );
-};
-
-const Sobremim = () => {
-  return (
-    <View style={styles.pagina2}>
-      <Text style={styles.images}><Image style={styles.images1} source={ logo }/> </Text>
-      <Text style={styles.textoPagina1}>
-      Olá, eu sou Luis Vinicius, estudante da área de programação e design e aluno do curso de Analise e Desenvolvimento de Sistemas através do SENAC (Embarque Digital) em Recife, Pernambuco.{"\n"}
-      Amante de tecnologia, Jogos são o meu hobby, alem de livros e filmes. Atualmente, sou entusiasta na área de programação e banco de dados, masterizando linguagens como C# e JavaScript e aprendendo cada dia mais sobre toda a area de tecnologia.{"\n"}
-      </Text>
-    </View>
-  );
-};
-
-const Habilidades = () => {
-  return (
-    <View style={styles.pagina}>
-      <Text style={styles.textoPagina1}>
-      {"\n"}<Text style={styles.textoPagina}>LINGUAGENS</Text>{"\n"}
-        HTML, CSS, JavaScript.{"\n"}
-        Python.{"\n"}
-        C# - Iniciante. {"\n"}
-      {"\n"}<Text style={styles.textoPagina}>SOFT SKILLS</Text>{"\n"}
-        Comunicação. {"\n"}
-        Resiliência. {"\n"}
-        Trabalho em equipe. {"\n"}
-        Empatia.{"\n"}
-      {"\n"}<Text style={styles.textoPagina}>IDIOMAS</Text>{"\n"} 
-        Portugues - Fluente. {"\n"}
-        Ingles - Avançado.
-      </Text>
-    </View>
-  );
-};
-
-const Contatos = () => {
-  return (
-    <View style={styles.pagina}>
-      <Text style={styles.textoPagina}>
-      <Image style={styles.images2} source={ logo1 }/> {"\n\n\n\n"}
-      {"\n"}           Luis Vinicius F. Silva{"\n"}
-        {"\n"}     Telefone: (81) 99805-4372
-        {"\n"}Email: Luis.silva@edu.pe.senac.br
-        {"\n"}          Recife, Pernambuco.
-
-      </Text>
-    </View>
-  );
-};
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
 const App = () => {
-  const [menuAberto, setMenuAberto] = useState(false);
-  const [paginaAtual, setPaginaAtual] = useState('Página Inicial');
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieData, setMovieData] = useState(null);
+  const [location, setLocation] = useState(null);
 
-  const toggleMenu = () => {
-    setMenuAberto(!menuAberto);
-  };
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permissão de localização não concedida', 'Por favor, conceda permissão de localização para obter a localização.');
+        return;
+      }
 
-  const navegarPara = (pagina) => {
-    setPaginaAtual(pagina);
-    setMenuAberto(false);
-  };
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData);
+    })();
+  }, []);
 
-  // Renderização condicional da página atual
-  const renderizarPagina = () => {
-    switch (paginaAtual) {
-      case 'Página Inicial':
-        return <PaginaInicial />;
-      case 'Sobre Mim':
-        return <Sobremim />;
-      case 'Habilidades':
-        return <Habilidades />;
-      case 'Meus Contatos':
-        return <Contatos />;
-      default:
-        return null;
+  const handleSearch = async () => {
+    if (movieTitle.trim() === '') {
+      Alert.alert('Aviso', 'Por favor, insira um título de filme válido.');
+      return;
+    }
+    try {
+      const apiKey = '1993e977'; // Substitua pelo seu próprio API Key
+      const apiUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`;
+
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.Response === 'True') {
+        setMovieData(data);
+      } else {
+        Alert.alert('Erro', 'Filme não encontrado. Verifique o título e tente novamente.');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Houve um problema na busca do filme. Tente novamente mais tarde.');
     }
   };
 
   return (
     <View style={styles.container}>
-      {/* Cabeçalho */}
-      <View style={styles.header}>
-        <Text style={styles.titulo}>Meu Currículo</Text>
-        <TouchableOpacity onPress={toggleMenu} style={styles.menuIcon}>
-          <Text>☰</Text>
-        </TouchableOpacity>
-      </View>
+      <Text style={styles.title}>
+        Filmes e Localizações
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o nome do filme"
+        value={movieTitle}
+        onChangeText={(text) => setMovieTitle(text)}
+      />
+      <Button title="Buscar Filme" onPress={handleSearch} />
 
-      {/* Menu */}
-      {menuAberto && (
-        <View style={styles.menu}>
-          <TouchableOpacity style={styles.menuItem} onPress={() => navegarPara('Página Inicial')}>
-            <Text style={styles.menuItemText}>Página Inicial</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuItem} onPress={() => navegarPara('Sobre Mim')}>
-            <Text style={styles.menuItemText}>Sobre Mim</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuItem} onPress={() => navegarPara('Habilidades')}>
-            <Text style={styles.menuItemText}>Habilidades</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuItem} onPress={() => navegarPara('Meus Contatos')}>
-            <Text style={styles.menuItemText}>Meus Contatos</Text>
-          </TouchableOpacity>
+      {movieData && (
+        <View style={styles.movieDataContainer}>
+          <Text style={styles.subtitle}>{movieData.Title}</Text>
+          <Text>Ano: {movieData.Year}</Text>
+          <Text>Gênero: {movieData.Genre}</Text>
+          <Text>Diretor: {movieData.Director}</Text>
+          <Text>Prêmios: {movieData.Awards}</Text>
+        </View> )}
+
+      {location && (
+        <View style={styles.locationContainer}>
+          <Text style={styles.subtitle}>Veja Sua Localização Aqui!</Text>
+          <Text>Latitude: {location.coords.latitude}</Text>
+          <Text>Longitude: {location.coords.longitude}</Text>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="Você Esta Aqui!"
+            />
+          </MapView>
         </View>
       )}
-
-      {/* Conteúdo */}
-      <View style={styles.content}>{renderizarPagina()}</View>
-    </View>
-  );
+        </View>
+  ); 
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 24,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flex: 10,
     padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    backgroundColor: '#F5F5F5',
   },
-  titulo: {
-    fontSize: 20,
+  title: {
+    fontSize: 24, 
     fontWeight: 'bold',
+    color: '#1C1C1C',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    margin: 20,
+    padding: 15,
+    textAlign: 'center',
+    backgroundColor: '#836FFF',
+    borderRadius: 20,
   },
-  menuIcon: {
-    padding: 10,
+  input: {
+    borderWidth: 2,
+    margin: 5,
+    padding: 5,
+    borderRadius: 10,
   },
-  menu: {
-    backgroundColor: '#f9f9f9',
-    padding: 10,
-  },
-  menuItem: {
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  menuItemText: {
-    fontSize: 16,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
+  locationContainer: {
+    marginTop: 20,
+    width: '90%',
+    backgroundColor: '#836FFF',
+    borderRadius: 10,
+    elevation: 3,
     alignItems: 'center',
+    justifyContent: 'center'
   },
-  pagina: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: 150,
+  map: {
+    width: '100%',
+    height: 200,
+    marginTop: 10,
   },
-  pagina2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: 50,
+  movieDataContainer: {
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#836FFF',
+    borderRadius: 10,
+    elevation: 3,
+    alignItems: 'center'
   },
-  textoPagina: {
+  subtitle: {
     fontSize: 18,
-    fontWeight: 'bold',
-  },
-  textoPagina1: {
-    fontSize: 18,
-    flex: 1,
-    
-  },
-  images: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    width: 200, 
-    height: 200, 
-    marginRight: 20,
-  },
-  images1: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    width: 200, 
-    height: 200, 
-    marginRight: 50,
-  },
-  images2: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    width: 200, 
-    height: 200, 
-    
-  },
+    fontWeight: 'bold', },
 });
 
 export default App;
